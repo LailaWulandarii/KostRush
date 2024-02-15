dbf0cb2188c3a52ca2b9a4c2b109cc4d71dcfd42
LailaWulandarii <E41220418@student.polije.ac.id>
2024-02-15 15:30:04 +0700

    konfigurasi template bootstrap

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get($uri, $callback);
-Route::post($uri, $callback);
-Route::put($uri, $callback);
-Route::patch($uri, $callback);
-Route::delete($uri, $callback);
-Route::options($uri, $callback);
-
-Route::match(['get', 'post'], '/', function(){
+Route::get('/home', function () {
+    return view('home');
 });
-Route::any('/', function (){
-});
-
-// Route::get('/user', 'ManagementUserController@index');
-Route::resource('/user', 'ManagementUserController@index');
