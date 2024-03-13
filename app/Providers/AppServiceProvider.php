ab8951d204791c347abb33ab32a6f7f9e98440c5
LailaWulandarii <E41220418@student.polije.ac.id>
2024-03-14 00:33:58 +0700

    add active sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            $view->with('active', function ($url) {
+                return Request::is($url) ? 'active' : '';
+            });
+        });
     }
 }
