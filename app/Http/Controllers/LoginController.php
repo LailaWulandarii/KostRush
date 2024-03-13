7a83317ff1142aa6c99507d237ae33d455261a85
LailaWulandarii <E41220418@student.polije.ac.id>
2024-03-14 00:32:51 +0700

    add session in login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,25 +16,27 @@ class LoginController extends Controller
 
     public function login_proses(Request $request)
     {
+
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        $credentials = $request->only('email', 'password'); // ganti $data menjadi $credentials
-
+        $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            $request->session()->put('user_email', $request->email);
+
             return redirect()->route('home');
         } else {
             return redirect()->route('login')->with('failed', 'Email atau Password salah');
         }
     }
 
+
     public function logout()
     {
         Auth::logout();
         return redirect()->route('login')->with('success', 'Kamu berhasil logout');
     }
-
-
 }
