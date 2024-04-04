1b2dcef4806df6625eef688894bd985e76b113b4
LailaWulandarii <E41220418@student.polije.ac.id>
2024-04-04 07:47:55 +0700

    belajar API

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Kamar;
 use App\Models\Kost;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -17,14 +19,96 @@ class HomeController extends Controller
     public function index()
     {
         $data = User::get();
-        return view('index', compact('data'));
+        $data = [
+            'status' => 200,
+            'user' => $data
+        ];
+        return response()->json($data, 200);
         // return view('home');
     }
-    public function home()
+    public function store(Request $request)
     {
-        $data = User::get();
-        return view('pages.home', compact('data'));
-        // return view('home');
-    }
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:5|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', 'string', 'min:6', 'max:255', 'regex:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/'],
+            'alamat' => 'required|string|min:15|max:255',
+            'tgl_lahir' => 'required|date',
+            'no_hp' => ['required', 'string', 'max:15', 'regex:/^(08|\+62)\d{9,13}$/'],
+            'jenis_kelamin' => 'required',
+        ]);
 
+        if ($validator->fails()) {
+            $data = [
+                "status" => 422,
+                "message" => $validator->messages()
+            ];
+            return response()->json($data, 422);
+        } else {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->role = 'penyewa';
+            $user->alamat = $request->alamat;
+            $user->tgl_lahir = $request->tgl_lahir;
+            $user->no_hp = $request->no_hp;
+            $user->jenis_kelamin = $request->jenis_kelamin;
+            $user->save();
+
+            $data = [
+                'status' => 200,
+                'message' => 'Data sudah diupload'
+            ];
+            return response()->json($data, 200);
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:5|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', 'string', 'min:6', 'max:255', 'regex:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/'],
+            'alamat' => 'required|string|min:15|max:255',
+            'tgl_lahir' => 'required|date',
+            'no_hp' => ['required', 'string', 'max:15', 'regex:/^(08|\+62)\d{9,13}$/'],
+            'jenis_kelamin' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                "status" => 422,
+                "message" => $validator->messages()
+            ];
+            return response()->json($data, 422);
+        } else {
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->role = 'penyewa';
+            $user->alamat = $request->alamat;
+            $user->tgl_lahir = $request->tgl_lahir;
+            $user->no_hp = $request->no_hp;
+            $user->jenis_kelamin = $request->jenis_kelamin;
+            $user->save();
+
+            $data = [
+                'status' => 200,
+                'message' => 'Data sudah diupdate'
+            ];
+            return response()->json($data, 200);
+        }
+    }
+    public function destroy($id)
+    {
+
+        $user = User::find($id);
+        $user->delete();
+        $data = [
+            'status' => 200,
+            'message' => 'Data sudah dihapus'
+        ];
+        return response()->json($data, 200);
+    }
 }
