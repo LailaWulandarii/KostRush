f62ab308ebd6b77aa39e44c2fc675f151c71409a
LailaWulandarii <E41220418@student.polije.ac.id>
2024-05-03 14:52:09 +0700

    new route for transaksi

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::post('/login_proses', [LoginController::class, 'login_proses'])->name('lo
 //==============================================REGISTER==============================================//
 Route::get('/register_akun', [RegisterController::class, 'registerAkunForm'])->name('register_akun_form');
 Route::post('/register_akun', [RegisterController::class, 'registerAkun'])->name('register_akun_process');
+// Route::get('/verify-email/{token}', [RegisterController::class, 'verifyEmail'])->name('verify.email');
 //==============================================EMAIL VERIFY==============================================//
 Route::get('/email', [LoginController::class, 'email'])->name('email');
 //==============================================LOGOUT==============================================//
@@ -34,14 +35,16 @@ Route::middleware(['auth'])->group(function () {
     //==============================================KOST==============================================//
     Route::resource('kost', KostController::class)->only(['create', 'index', 'store', 'update']);
     //==============================================TRANSAKSI==============================================//
-    Route::resource('transaksi', TransaksiController::class)->only(['index']);
+    Route::get('/transaksi-baru', [TransaksiController::class, 'index'])->name('transaksi.baru');
+    Route::get('/transaksi-proses', [TransaksiController::class, 'indexProses'])->name('transaksi.proses');
     Route::put('/transaksi/{id_transaksi}/proses', [TransaksiController::class, 'prosesTransaksi'])->name('proses.transaksi');
     Route::put('/transaksi/{id_transaksi}/verifikasi', [TransaksiController::class, 'verifikasiTransaksi'])->name('verifikasi.transaksi');
     Route::get('/riwayat-transaksi', [TransaksiController::class, 'riwayat'])->name('riwayat.transaksi');
     //==============================================PROFIL==============================================//
     Route::get('/profil', [ProfileController::class, 'index'])->name('profil');
     Route::put('/profil/{id}', [ProfileController::class, 'update'])->name('profil.update');
-    Route::put('/profil/{id}/update-password', [ProfileController::class, 'updatePassword'])->name('profil.update-password');
+
+    Route::put('/profil/change-password', [ProfileController::class, 'changePassword'])->name('profil.change.password');
     Route::delete('/profil/{id}', [ProfileController::class, 'destroy'])->name('profil.destroy');
     //==============================================DLL==============================================//
     Route::get('/home', [HomeController::class, 'home'])->name('home');
