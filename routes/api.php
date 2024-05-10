73cfb37edef2b7cd166cd7441afc0e1a69a9e9ee
LailaWulandarii <E41220418@student.polije.ac.id>
2024-05-10 07:31:27 +0700

    api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PenghuniController;
@@ -9,6 +10,7 @@ use App\Http\Controllers\API\KostController;
 use App\Http\Controllers\API\ProfilController;
 use App\Http\Controllers\API\TransaksiController;
 use Tymon\JWTAuth\Facades\JWTAuth;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,43 +21,31 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::get('/profile', function () {
-//         // ...
-//     });
-//     Route::get('/posts', function () {
-//         // ...
-//     });
-// });
+
+//Auth
 Route::post('/apilogin', [AuthController::class, 'login']);
-Route::get('/apipenghuni', [PenghuniController::class, 'index']);
-Route::get('/apikost', [KostController::class, 'index'])->middleware('auth:sanctum');
-Route::get('/filterKostKecamatan', [KostController::class, 'filterKostKecamatan'])->middleware('auth:sanctum');
-Route::get('/apikamar', [KamarController::class, 'index'])->middleware('auth:sanctum');
-Route::get('/filterKamarKecamatan', [KamarController::class, 'filterKamarKecamatan'])->middleware('auth:sanctum');
-Route::get('/apitransaksi', [TransaksiController::class, 'index'])->middleware('auth:sanctum');
-Route::get('/apidetailprofil', [ProfilController::class, 'index'])->middleware('auth:sanctum');
-Route::put('/apieditprofil', [ProfilController::class, 'update'])->middleware('auth:sanctum');
+Route::post('/apiregister', [AuthController::class, 'register']);
+Route::post('/apilogout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// Route::get('/kost/filter-by-kecamatan/{kecamatan}', 'API\KostController@filterKostByKecamatan');
+//Penghuni
+Route::apiResource('apipenghuni', PenghuniController::class);
 
+//Kamar
+Route::apiResource('apikamar', KamarController::class);
+Route::post('filterkamarkecamatan', [KamarController::class, 'filterKamarKecamatan']);
+Route::get('kamartermurah', [KamarController::class, 'kamarTermurah']);
+Route::get('kamarterpopuler', [KamarController::class, 'kamarTerpopuler']);
+Route::get('kamarkosong', [KamarController::class, 'kamarKosong']);
 
-// // Tambahkan rute yang menggunakan JWT middleware
-// Route::middleware('jwt.auth')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+//Kost
+Route::get('apikost', [KostController::class, 'index']);
+Route::post('filterkostkecamatan', [KostController::class, 'filterKostKecamatan']);
+Route::post('searchkost', [KostController::class, 'searchKost']);
 
-// Route::get('/coba', function () {
-//     return 'halo, saya belajar api';
-// });
+//Transaksi
+Route::get('apitransaksi', [TransaksiController::class, 'index'])->middleware('auth:sanctum');
+Route::post('apiaddtransaksi', [TransaksiController::class, 'addTransaksi'])->middleware('auth:sanctum');
 
-// Route::apiResource('apipenghuni', PenghuniController::class)->middleware('auth:api');
-// Route::post('/apiregister', [AuthController::class, 'register']);
-// // Route::resource('loginAPI', HomeController::class);
-
-// // Route untuk login pengguna
-// Route::post('/apilogin', [AuthController::class, 'login']);
-
-// // Route untuk logout pengguna
-// Route::post('/apilogout', [AuthController::class, 'logout'])->middleware('auth:api');
-// Route::get('/apipenghuni', [AuthController::class, 'login']);
+//Profile
+Route::get('apigetprofile', [ProfileController::class, 'index'])->middleware('auth:sanctum');
+Route::put('apieditpassword', [ProfileController::class, 'updatePassword'])->middleware('auth:sanctum');
